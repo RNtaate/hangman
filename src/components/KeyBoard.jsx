36cf36f1c94698940a5b36f42798728f3827ea85
RNtaate <royhyde14@gmail.com>
2024-11-14 00:09:04 +0300

    Create, design and style a keyboard component that will b used in the game page

---
+++ src/components/KeyBoard.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import letterKeys from '../services/letters';
+
+const KeyBoard = () => {
+  const keysArray = Object.keys(letterKeys);
+  return (
+    <section className="min-w-[320px] w-full p-4">
+      <div className="w-full flex flex-wrap gap-3 justify-center py-3 bg-white shadow-md rounded-md">
+        {keysArray.map((singleKey) => {
+          return (
+            <button
+              type="button"
+              key={singleKey}
+              className="w-[13%] max-w-[50px] aspect-square bg-sky-500 text-white disabled:opacity-[0.4] shadow-md rounded-full"
+              disabled={singleKey == 'A' || singleKey == 'T' ? true : false}
+            >
+              {letterKeys[singleKey]}
+            </button>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default KeyBoard;
