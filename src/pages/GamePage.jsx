ddb13b9b02ca177049cc927536d5d969e37249a7
RNtaate <royhyde14@gmail.com>
2024-11-14 00:08:20 +0300

    Create a game page

---
+++ src/pages/GamePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import KeyBoard from '../components/KeyBoard';
+
+const GamePage = () => {
+  return (
+    <div>
+      GamePage
+      <KeyBoard />
+    </div>
+  );
+};
+
+export default GamePage;
