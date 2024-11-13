930121408456af9d83f8722fd913d54c516727ad
RNtaate <royhyde14@gmail.com>
2024-11-13 17:46:32 +0300

    Install and configure tailwind css in the project.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return <h1>Home page</h1>;
+  return <h1 className="text-3xl font-bold">Home page</h1>;
 }
 
 export default App;
