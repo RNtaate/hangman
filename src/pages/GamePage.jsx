9d4d9ed794ad495c0d283db473139418a4a6c4ea
RNtaate <royhyde14@gmail.com>
2024-11-21 02:49:53 +0300

    Use the gameOver, winStatus state in the GamePage component.

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 import KeyBoard from '../components/KeyBoard';
 import HangmanCanvas from '../components/HangmanCanvas';
 import PlayedWord from '../components/PlayedWord';
+import { getContextValues } from '../context/GameContextProvider';
 
 const GamePage = () => {
+  const { gameOver, winStatus, wrongGuessCount } = getContextValues();
+
   return (
-    <div className="flex flex-col md:justify-around md:min-h-screen md:max-h-screen">
+    <div className="flex flex-col md:justify-around md:min-h-screen md:max-h-screen relative">
+      {gameOver && (
+        <div className="absolute top-0 left-0 right-0 bottom-0">
+          GAME OVER
+          <p>{winStatus}</p>
+        </div>
+      )}
       <HangmanCanvas />
       <PlayedWord />
       <KeyBoard />
