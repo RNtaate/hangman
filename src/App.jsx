bc60820e4809849e3550c340b1924346cdff05ea
RNtaate <royhyde14@gmail.com>
2024-11-21 16:04:27 +0300

    Use react router-dom to create navigation in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 import { useState } from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
 import WordInput from './components/WordInput';
+import GamePage from './pages/GamePage';
+import GameContextProvider from './context/GameContextProvider';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <HomePage />,
+    },
+    {
+      path: '/gamepage',
+      element: <GamePage />,
+    },
+  ]);
+
   return (
-    <div className="min-w-[360px] max-w-[500px] relative min-h-screen max-h-screen overflow-hidden">
-      <HomePage />
-      <WordInput />
-    </div>
+    <GameContextProvider>
+      <div className="min-w-[360px] max-w-[500px] relative min-h-screen max-h-screen overflow-hidden">
+        <RouterProvider router={router} />
+      </div>
+    </GameContextProvider>
   );
 }
 
